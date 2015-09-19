e1ba361d291ff60f792096b76e271af9f693416d
Timo Schwarzer <timo@setagon.com>
2015-09-19 19:02:32 +0200

    va -> middle [2]

--- inc/template.php
+++ inc/template.php
@@ -70,7 +70,7 @@ include "inc/session.php";
 	} else {
 	?> <nav id="header">
 		<div id="logo">
-			<a href="#"><img style="padding: 10px; max-width: 100px; vertical-align: middle;" src="img/logo.png" alt="" />></a>
+			<a href="#"><img style="padding: 10px; vertical-align: middle;" src="img/logo.png" alt="" /></a>
 		</div>
 		<!--<a href="logout">logout</a>-->
 		<div id="username">
