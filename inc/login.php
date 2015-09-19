bc9402c4651f0fe7a4dba4e22bcb0d5bbed2411e
René Veenhuis <re.veenhuis@gmail.com>
2015-09-19 16:18:42 +0200

    Login - Final

--- inc/login.php
+++ inc/login.php
@@ -33,7 +33,14 @@
 <div class="row">
   <div class="col-md-4"></div>
   <div class="col-md-4">
-  	<button type="button" class="btn btn-default"><? echo "hi"; ?></button>
+  	<form class="form-signin" style="margin-top: 50%; padding: 30px; display: block; background-color: white; border: 1px solid #ccc; border-radius:6px; vertical-align: middle; opacity=0.1;">
+        <h2 class="form-signin-heading">Anmelden</h2>
+        <label for="inputEmail" class="sr-only">E-Mail Adresse</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 20px;">Anmelden</button>
+      </form>
   </div>
   <div class="col-md-4"></div>
 </div>
