0e5efecaad3e526906751512efe974c6d47863bb
René Veenhuis <re.veenhuis@gmail.com>
2015-09-19 19:48:58 +0200

    mit JS

--- inc/template.php
+++ inc/template.php
@@ -12,7 +12,8 @@ include "inc/session.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<title>SchoolNet</title>
-
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery-ui.js"></script>
 	<!-- Bootstrap -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	
