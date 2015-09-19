f5f49add1ed68d5f0d5c454c284024892b74bde2
Jonas Lührig <jonas.luehrig@gmail.com>
2015-09-19 18:28:08 +0200

    add mainContainer´

--- inc/template.php
+++ inc/template.php
@@ -15,7 +15,7 @@ function loadPage($page){
 
 	<!-- Bootstrap -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css">
+	<link href="css/style.css" rel="stylesheet">
 
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
