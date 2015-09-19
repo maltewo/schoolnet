4fafcca88670ad4e4cc1699f2d929517e2906a3d
Timo Schwarzer <timo@setagon.com>
2015-09-19 16:35:48 +0200

    import Materialize

--- inc/template.php
+++ inc/template.php
@@ -14,6 +14,7 @@ function templateStart() {
 	<title>SchoolNet</title>
 
 	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css">
 
