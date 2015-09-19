cebc59d0e28d18c422bea7da323415759b6e16a2
Timo Schwarzer <timo@setagon.com>
2015-09-19 16:45:58 +0200

    Set the max with of login box

--- login.php
+++ login.php
@@ -35,7 +35,8 @@
 
 
 <div class="row">
-  <div class="center" style="max-width: 600px;">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">
   	<form class="form-signin" style="margin-top: 50%; padding: 30px; display: block; background-color: rgba(255,255,255,0.8); border: 1px solid rgba(204,204,204,0.9); border-radius:6px; vertical-align: middle; opacity=0.1;">
         <h2 class="form-signin-heading">Anmelden</h2>
         <label for="inputEmail" class="sr-only">E-Mail Adresse</label>
@@ -45,6 +46,7 @@
         <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 20px;">Anmelden</button>
       </form>
   </div>
+  <div class="col-md-4"></div>
 </div>
 
 <?
