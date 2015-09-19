a635258296eebf849c784f175463b18e63980d3f
Peter Mader <klopstedt@hack-hro.de>
2015-09-19 17:48:48 +0200

    anpassung overview.php an template engine

--- inc/overview.php
+++ inc/overview.php
@@ -1,1 +1,23 @@
-distanz zwischen objekten
+<div class="row">
+  <div class="col-md-3"></div>
+  <div class="col-md-6">
+  	<div class="page-header">
+      <h1>Example page header</h1>
+    </div>
+    <div class="list-group">
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Aufgabe 1</h4>
+        <p class="list-group-item-text"></p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Aufgabe 2</h4>
+        <p class="list-group-item-text"></p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Aufgabe 3</h4>
+        <p class="list-group-item-text"></p>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3"></div>
+</div>
