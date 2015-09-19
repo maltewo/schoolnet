1f2d15de695442591fa2e81a20f74ab82b523150
Jan Moritz Meyer <janmoritzmeyer@jm-mac-book-pro.speedport_w723_v_typ_a_1_00_096>
2015-09-19 20:29:25 +0200

    exercise.result und submit erstellt

--- inc/sites/exercise_submit.php
+++ inc/sites/exercise_submit.php
@@ -6,16 +6,18 @@
 	<h1>Aufgabe X</h1>
 	<h2>Aufgabenstellung:</h2>
 	<p>Aufgabenstellung als Fließtext jhdajfhksdhgkjhjfdhjgkjharfjgjhjrkthjafjghkafjdhhafgk hjgf jagf dkjghjafdhgj afjdk gjafd kgjhakfghkjafhjgh ajkghkjhfa gkhafk hfag fgjkhgfkjah fjhg jkahgjhagj</p>
-	<h2>Lösungen:</h2>
-	
-	<!-- Zu wiederholende Schleife -->
-	<div class="loesung">
-		<h3>Max Mustermann</h3>
-		<p>Lösungen als Fließtext kjadkfksdabjjasdjfgf jsad kfj asdfgjakjgka hfugjkahdf jgkaf sjhga hagjf hkfg kjahjkahgjfhkjgfhjh kj </p>
+
+	<div class="form-group">
+		<label for="question">Lösung:</label>
+		<textarea class="form-control" rows="10" id="question"></textarea>
 	</div>
 	
-
-
+	<div class="row" style="margin-top: 20px;">
+			<div class="col-md-12">
+				<p class="center"><button type="button" class="btn btn-success btn-lg btn-block">Senden</button></p>
+			</div>
+		</div>
+	
   </div>
   <div class="col-md-3"></div>
 </div>
