6e831fb81a5ca64b77fccf7e9ba746a94a426454
René Veenhuis <re.veenhuis@gmail.com>
2015-09-20 00:17:38 +0200

    Template

--- inc/template.php
+++ inc/template.php
@@ -18,6 +18,16 @@
 		public function loadPage() {
 			return true;
 		}
+
+		public function setBody() {
+			$page = $this->page;
+
+			if($page == "login" || $page == "stundenplan") {
+				return '<body background = "img/background.jpg" style = "background-size: cover">';
+			} else {
+				return '<body>';
+			}
+		}
 	}
 
 	$template = new Template();
@@ -32,26 +42,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>SchoolNet</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	
-		<?php echo $template->loadNavigation() ?>
-
 	<link href="css/style.css" rel="stylesheet">
 	<link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+	<script src="/js/jquery.js" type="text/javascript"></script>	
+
+	<!--Navigation laden-->
+	<?php echo $template->loadNavigation() ?>
 
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-	<script src="/js/jquery.js" type="text/javascript"></script>
+
 </head>
-	<?php
-		if($_GET['page'] == "login" || $_GET['page'] == "stundenplan") {
-			echo '<body background = "img/background.jpg" style = "background-size: cover">';
-		} else {
-			echo '<body>';
-		}
-	?>
-	<body>
+
+	<?php $template->setBody(); ?>
+
 
 	<?php if($_GET['page'] == "login") {
 	?>
