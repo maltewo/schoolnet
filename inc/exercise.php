daf3121fb5fdb551992749826771a0944efc928a
lukas <lukas@192.168.81.243>
2015-09-20 00:45:06 +0200

    test

--- inc/exercise.php
+++ inc/exercise.php
@@ -71,7 +71,7 @@ function getExerciseById($exerciseId) {
 
 function getAnswerById($pId) {
 	echo "SELECT * FROM ANSWERS WHERE ID=".$pId;
-	$lResponse = dbQuery("SELECT * FROM ANSWERS WHERE ID='%s'", $pId)->fetch_assoc();
+	$lResponse = dbQuery("SELECT * FROM ANSWERS WHERE ID=1")->fetch_assoc();
 	echo "<pre>".var_dump($mAnswer)."</pre>";
 	if (getUserId() == $lResponse["OWNER"]) {
 		return new Answer($pId, $lResponse["TEXT"], $lResponse["OWNER"]);
