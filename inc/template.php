1e78cf0b0b963d8fd665a87a9cb391d29edbf43b
Jonas Lührig <jonas.luehrig@gmail.com>
2015-09-19 20:47:06 +0200

    finalized template

--- inc/template.php
+++ inc/template.php
@@ -26,6 +26,7 @@ include "inc/session.php";
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	<script src="/js/jquery.js" type="text/javascript"></script>
 </head>
 	<?php
 		if($_GET['page'] == "login") {
