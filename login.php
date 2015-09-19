eb74b59cb0dcc70b87a381da01c233eb807f6517
Timo Schwarzer <timo@setagon.com>
2015-09-19 16:34:28 +0200

    center div

--- login.php
+++ login.php
@@ -35,8 +35,7 @@
 
 
 <div class="row">
-  <div class="col-md-4"></div>
-  <div class="col-md-4">
+  <div class="col-md-4 center">
   	<form class="form-signin" style="margin-top: 50%; padding: 30px; display: block; background-color: rgba(255,255,255,0.8); border: 1px solid rgba(204,204,204,0.9); border-radius:6px; vertical-align: middle; opacity=0.1;">
         <h2 class="form-signin-heading">Anmelden</h2>
         <label for="inputEmail" class="sr-only">E-Mail Adresse</label>
@@ -46,7 +45,6 @@
         <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: 20px;">Anmelden</button>
       </form>
   </div>
-  <div class="col-md-4"></div>
 </div>
 
 <?
