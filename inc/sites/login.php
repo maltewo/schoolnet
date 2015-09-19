cab70c59ee2d6fc69c53b80a4f61b877e38290f0
Peter Mader <klopstedt@hack-hro.de>
2015-09-19 20:52:15 +0200

    neues design errormessag

--- inc/sites/login.php
+++ inc/sites/login.php
@@ -31,12 +31,17 @@ if ($isLoggingIn) {
 <div class="row">
   <div class="col-md-4"></div>
   <div class="col-md-4">
+    <?php
+      if ($errorOccurred) echo "<div class='alert alert-danger' role='alert'>$errorMessage</div>";
+      if (notNull($message)) echo "<h3 style='color: green;'>$message</h3>";
+    ?>
+
     <form class="form-signin" method="post" action="index.php?page=login" style="margin-top: 50%; padding: 30px; display: block; background-color: rgba(255,255,255,0.8); border: 1px solid rgba(204,204,204,0.9); border-radius:6px; vertical-align: middle; opacity=0.1;">
         <h2 class="form-signin-heading">Anmelden</h2>
         <?php
 
-        if ($errorOccurred) echo "<h3 style='color: red;'>$errorMessage</h3>";
-        if (notNull($message)) echo "<h3 style='color: green;'>$message</h3>";
+        //if ($errorOccurred) echo "<h3 style='color: red;'>$errorMessage</h3>";
+        //if (notNull($message)) echo "<h3 style='color: green;'>$message</h3>";
         ?>
         <input type="hidden" name="login" value="login">
         <input type="text" name="account" class="form-control" placeholder="Benutzername" required autofocus>
