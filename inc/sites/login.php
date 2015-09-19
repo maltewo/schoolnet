0be3ff2aca1f08681905a186d0a94e0ed69ab273
Peter Mader <klopstedt@hack-hro.de>
2015-09-19 21:46:58 +0200

    fixed login + merge

--- inc/sites/login.php
+++ inc/sites/login.php
@@ -36,14 +36,12 @@ if ($isLoggingIn) {
 <div class="row">
   <div class="col-md-offset-4 col-md-4">
     <?php
-      if ($errorOccurred) { echo "<div class='alert alert-danger' role='alert' style='position: absolute; top: 45%; width: 95.5%'>$errorMessage</div>"; }
+      if ($errorOccurred) echo "<div class='alert alert-danger' role='alert' style='position: absolute; top: 45%; width: 95.5%'>$errorMessage</div>";
+      elseif(isset($message)) echo "<div class='alert alert-success' role='alert' style='position: absolute; top: 45%; width: 95.5%'>$message</div>";
     ?>
 
     <form class="form-signin" method="post" action="index.php?page=login" style="margin-top: 50%;padding: 30px; display: block; background-color: rgba(255,255,255,0.8); border: 1px solid rgba(204,204,204,0.9); border-radius:6px; vertical-align: middle; opacity=0.1;">
         <h2 class="form-signin-heading">Anmelden</h2>
-        <?php
-        if (isset($message)) echo "<h3 style='color: green;'>$message</h3>";
-        ?>
         <input type="hidden" name="login" value="login">
         <input type="text" name="account" class="form-control" placeholder="Benutzername" required autofocus>
         <input type="password" name="password" class="form-control" placeholder="Passwort" required>
