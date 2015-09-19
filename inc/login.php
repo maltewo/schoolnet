62cb2f46430e9aceacd81e801d671ec8d60e09ab
René Veenhuis <re.veenhuis@gmail.com>
2015-09-19 15:59:42 +0200

    Login#1

--- inc/login.php
+++ inc/login.php
@@ -1,21 +1,33 @@
-<?
-	
-?>
+<body background = "img/background.jpg" style = "background-size: cover">
 
-<nav class="navbar navbar-inverse">
-	<div class="navbar-header">
-  		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-9" aria-expanded="false">
-   			<span class="sr-only">Toggle navigation</span>
-    		<span class="icon-bar"></span>
-    		<span class="icon-bar"></span>
-  			<span class="icon-bar"></span>
-  		</button>
-  <a class="navbar-brand" href="#">SchoolNet</a>
-</div>
-</ul>
-</nav>
-
-<img src="background.jpg"></img>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">SchoolNet</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
 
 
 <div class="row">
