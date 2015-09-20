da8009f828e42d8270d31e7fa4cd2e7ccf62fad9
Timo Schwarzer <timo@setagon.com>
2015-09-20 12:27:08 +0200

    update overview 5

--- inc/sites/overview.php
+++ inc/sites/overview.php
@@ -27,7 +27,8 @@ if (isset($_GET["delId"])) {
                   ?>
                   <a href="?page=exercise_results&amp;id=<?php echo $mExercise->mId; ?>"
                      class="list-group-item excercise-list">
-                    <h4 class="list-group-item-heading"><?php echo htmlspecialchars($mExercise->mTitle); ?></h4>
+                    <h4><?php echo htmlspecialchars($mExercise->mTitle); ?></h4>
+                    <a style="position: absolute; text-align: right;" href="?page=overview&delId=<?php echo $mExercise->mId?>" class="glyphicon glyphicon-remove pull-right"></a>
                   </a>
                   <?php
                 }
