b8b885f9e36feb7d172f651dbe1588a956385a23
René Veenhuis <re.veenhuis@gmail.com>
2015-09-20 10:56:20 +0200

    Local

--- inc/template.php
+++ inc/template.php
@@ -67,7 +67,7 @@
 
 	<!--Navigation laden-->
 	<?php 
-	echo $template->loadNavigation() ?>
+	echo $template->setNavi(); ?>
 
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
