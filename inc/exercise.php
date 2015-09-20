63575d5ec6045bea0fc84dac5be84885f6340f2b
lukas <lukas@netbook>
2015-09-20 11:55:01 +0200

    test

--- inc/exercise.php
+++ inc/exercise.php
@@ -52,7 +52,6 @@ function deleteExercise($exerciseId) {
 }
 
 function addAnswer($pText, $pExercise) {
-	echo "INSERT INTO ANSWERS (TEXT, OWNER, EXERCISE, GROUP) VALUES ('".$pText."', ".getUserId().", ".$pExercise.", ".$_SESSION["group"];
 	dbQuery("INSERT INTO ANSWERS (`TEXT`, OWNER, EXERCISE, `GROUP`) VALUES ('%s', %s, %s, %s)", $pText, getUserId(), $pExercise, $_SESSION["group"]);
 }
 
