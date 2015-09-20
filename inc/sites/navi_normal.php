3e2be3b206bd0d8fb5d4814b93737838183f876c
Timo Schwarzer <timo@setagon.com>
2015-09-20 10:35:15 +0200

    updated navi 9

--- inc/sites/navi_normal.php
+++ inc/sites/navi_normal.php
@@ -6,7 +6,7 @@
 
 		<div id="username">
 			<p style="display: inline"><?php echo $_SESSION["username"]; ?></p>
-			<a href="index.php?page=stundenplan" class="btn navbutton">Vertretungsplan</a>
-			<a href="index.php?page=login&logout=true" class="btn navbutton">Abmelden</a>
+			<a href="index.php?page=stundenplan" class="btn btn-primary">Vertretungsplan</a>
+			<a href="index.php?page=login&logout=true" class="btn btn-primary">Abmelden</a>
 		</div>
 	</nav>
