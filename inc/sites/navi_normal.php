877bbff6024d2ca955fe3ab41cf8ea659f633e2c
Malte Worat <malteworat@htp-tel.de>
2015-09-20 09:13:59 +0200

    Button Stundenplan created

--- inc/sites/navi_normal.php
+++ inc/sites/navi_normal.php
@@ -3,6 +3,8 @@
 			<a href="index.php"><img style="padding: 10px; vertical-align: middle;" src="img/logo.png" alt="" /></a>
 		</div>
 		<!--<a href="logout">logout</a>-->
+		
+		<a class="btn btn-default" href="stundenplan" role="button">Link</a>
 		<div id="username">
 			<p style="display: inline"><?php echo $_SESSION["username"]; ?></p>
 			<input id="logout" type="submit" value="Logout" onclick="<?php redirectToInline('index.php?page=login&logout=true'); ?>"/>
